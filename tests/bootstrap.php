34bf6425c978ce25b4f30d8cf1f7f392d403d68f
Anton Ukhanev <xedin.unknown@gmail.com>
2020-05-14 02:25:54 +0200

    Improve test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
 (function (string $basePath) {
-    $baseDir = dirname($basePath);
-    define('ROOT_DIR', $baseDir);
+    $baseDir = dirname($basePath, 2);
+    define('ROOT_DIR', "$baseDir/../");
 
     /** @noinspection PhpIncludeInspection */
     require_once "$baseDir/vendor/autoload.php";
